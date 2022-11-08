dff2cd751615dfc8979ee0a5c3fd56085fd53a78
rajeswarimandadhi <rajeswarimandadhi6@gmail.com>
2022-11-08 17:22:40 +0530

    topic

---
+++ SubscriptionReciever/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using System;
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+
+client = new ServiceBusClient("Endpoint=sb://myportfolios.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RLgvud41fAhu4K0XRk+2MJgPufXAS2BXT/0b6bCjMLs=");
+processor = client.CreateProcessor("mytopic", "s2",new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Lets wait for a minute and then you can press a key to end the processing..!!");
+    Console.ReadKey();
+    Console.WriteLine("\n Stopping the reciever");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("stopped recieving messages");
+
+}
+finally
+{
+
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($" recieved:{body} from subscription");
+    await args.CompleteMessageAsync(args.Message);
+
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
---
+++ TopicSender/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using System;
+
+//creating a Client Object
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+// Creating a sender object
+
+//No of Messages
+
+const int noOfMessages = 3;
+
+//initilizing Client and Sender with Azure credentials
+
+client = new ServiceBusClient("Endpoint=sb://myportfolios.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RLgvud41fAhu4K0XRk+2MJgPufXAS2BXT/0b6bCjMLs=");
+sender = client.CreateSender("mytopic");
+//Creating batch
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+    for (int i = 0; i <= noOfMessages; i++)
+    { 
+        if(!messageBatch.TryAddMessage(new ServiceBusMessage($"Message{i}")))
+
+        {
+            throw new Exception($"the message{i} is to large to fit in the batch");
+        }
+    }
+    try
+    {
+        await sender.SendMessagesAsync(messageBatch);
+        Console.WriteLine($"A Batch of {noOfMessages} has been published to the object");
+    }
+    finally
+    {
+        await sender.DisposeAsync();
+        await client.DisposeAsync();
+    }
+    Console.WriteLine("Press any key to stop the Execution");
+    Console.ReadKey();
+
+
