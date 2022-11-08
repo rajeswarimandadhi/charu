fb0e95a528c3cc45122b01e06213e7bebbf2fdc2
rajeswarimandadhi <rajeswarimandadhi6@gmail.com>
2022-11-08 17:16:16 +0530

    queue

---
+++ queueReciever/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+using Azure.Messaging.ServiceBus;
+
+using Microsoft.Extensions.Azure;
+
+
+
+ServiceBusClient client;
+
+
+
+ServiceBusProcessor processor;
+
+
+
+var clientOptions = new ServiceBusClientOptions
+
+{
+
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+
+};
+
+
+
+client = new ServiceBusClient("Endpoint=sb://cakeshop.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WedJwIXsPiLaZttjFRnmN2QbsM1yKH2u+Ew2EHOt9hc=", clientOptions);
+
+processor = client.CreateProcessor("myqueue", new ServiceBusProcessorOptions());
+
+
+
+
+
+try
+
+{
+
+    processor.ProcessMessageAsync += MessageHandler;
+
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+
+
+    Console.WriteLine("Wait for a minuteand then press key to end the processing...!!!");
+
+    Console.ReadKey();
+
+
+
+
+
+    Console.WriteLine("\n Stopping the reciever ...!!!");
+
+    await processor.StopProcessingAsync();
+
+    Console.WriteLine("Stopped recieving message .!!!!");
+
+}
+
+
+
+finally
+
+{
+
+    await processor.DisposeAsync();
+
+    await client.DisposeAsync();
+
+
+
+}
+
+
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+
+{
+
+    string body = args.Message.Body.ToString();
+
+    System.Console.WriteLine($"Recieved:{body}");
+
+
+
+    await args.CompleteMessageAsync(args.Message);
+
+}
+
+
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+
+{
+
+    Console.WriteLine(args.Exception.ToString());
+
+    return Task.CompletedTask;
+
+}
+
+
---
+++ queueSender/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+using System;
+
+
+        ServiceBusClient client;
+
+        ServiceBusSender sender;
+
+
+        const int no_of_messages = 3;
+        var clientOptions = new ServiceBusClientOptions
+        {
+            TransportType = ServiceBusTransportType.AmqpWebSockets
+        };
+
+        client = new ServiceBusClient("Endpoint=sb://cakeshop.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WedJwIXsPiLaZttjFRnmN2QbsM1yKH2u+Ew2EHOt9hc=", clientOptions);
+        sender = client.CreateSender("myqueue");
+
+        //creating a batch
+
+        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+        for (int i = 0; i < no_of_messages; i++)
+        {
+            //try adding a message to the batch
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+            {
+                throw new Exception($"The Message is too large to fit in the batch");
+            }
+        }
+        try
+        {
+            await sender.SendMessagesAsync(messageBatch);
+            Console.WriteLine($"A Batch of {no_of_messages} has been published to the Queue");
+        }
+        finally
+        {
+            //Need to cleanup objects and Resources in the network
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+
+        }
+        Console.WriteLine("press any key to close the application");
+        Console.ReadKey();
+    
